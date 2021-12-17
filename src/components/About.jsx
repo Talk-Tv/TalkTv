5239b45c3612016adc24c5941e9f35ec8bcf51d8
cstudio7 <conquerorsword36@yahoo.com>
2021-12-17 13:09:30 +0100

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import banner1 from "../img/headset.jpg";
+import banner1 from "../img/event poster/XPERINCE.png";
+import banner2 from "../img/event poster/yMR.png";
+import banner3 from "../img/event poster/wafBEC-2021.png";
+import banner4 from "../img/event poster/SOUR-AWARD.png";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 import SwiperCore, { Navigation, Pagination, Controller, Thumbs, Autoplay } from "swiper";
@@ -28,15 +31,12 @@ export default class About extends Component {
               <div className="talk">Our Story</div>
               <div className="text">
                 <div className="heading">
-                  TalkTv is your number one leading gospel entertainment network in Africa.
+                  Talk Tv Gospel Entertainment is a mandate given to our founder to create a space for the gospel world were believer can grow in the light of God
                 </div>
                 <div className="card-body">
-                  We are the number one leading gospel entertainment network in Africa,
-                  the mandate to reach the world with relatable and inspiring contents was launched in June 17 2020.
-                  We cover everything gospel from music, to events, to sermons, to our own spirit inspired content,
-                  we are an online television network and we are across all social platforms from YouTube, Facebook, Instagram, twitter and Tiktok
-                  <br />
-                  Read articles and trending topics in the health space
+                  We are going to achieve this through Fun and exciting and entertaining Christian relatable contents.
+                  We also function in giving the latest Gosoel News and Gist. Conferences and Gospel Event Updates. Event Coverage.
+                  Gospel Entertainment and Lifestyle News and Updates about your favorite preachers. Gospel Music Artist and so much more. We thrive to be the best Gospel Entertainment Platform in Africar not just Nigeria.
                 </div>
               </div>
             </div>
@@ -82,11 +82,10 @@ export default class About extends Component {
                       src={banner1}
                       alt="news"
                   />
-                  <h3>Frank Edward</h3>
+                  <h3>The Experience</h3>
                   <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Dolorum neque tenetur facta ncing elit.
-
+                    This 2021, The Experience will be the 16th edition of the gospel concert and promises to exceed expectations.
+                    The event will hold at The Rock Cathedral (registration required) and be live-streamed on all social media platforms.
                   </p>
                 </div>
               </SwiperSlide>
@@ -95,13 +94,13 @@ export default class About extends Component {
                 <div className="artist__content">
                   <img
                       className="taltv__img"
-                      src={banner1}
+                      src={banner2}
                       alt="news"
                   />
-                  <h3>Frank Edward</h3>
-                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Dolorum neque tenetur facilis corporis ratione fugiat quaerat consequatur nam, magni expedita necessitatibusLorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  <h3>YMR</h3>
+                  <p>
+                    Young Ministers Retreat (YMR 2021) is an annual revival program organized to set men on fire and equip men for the end time move of God. The 72 hour program is filled with a mix of intense prayers, fiery worship, evangelistic outreach, Impartations and transforming word sessions.
+                    It is hosted by the Amazing Pastor Daniel Olawande
                    </p>
                 </div>
               </SwiperSlide>
@@ -110,14 +109,18 @@ export default class About extends Component {
                 <div className="artist__content">
                   <img
                       className="taltv__img"
-                      src={banner1}
+                      src={banner3}
                       alt="news"
                   />
-                  <h3>Frank Edward</h3>
-                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Dolorum neque tenetur facilis corporis ratione fugiat quaerat consequatur nam, magni expedita necessitatibusLorem, ipsum dolor sit amet consectetur adipisicing elit.
-                     necessitatibusLorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  <h3>WAFBEC 2021</h3>
+                  <p>
+                    The 2021 edition of the West Africa Faith Believers Convention (WAFBEC) organised by Covenant Nation will be taking place from the 3rd - 8th of January 2021.
+
+                    The theme of the event is "Manifesting His Kingdom"
+
+                    Established in 2013, WAFBEC is a faith conference born in worship and intercession designed to address eternal issues and also things that are going on in time. It is prophetic, relevant and an impartation of grace. It is a time of prayer, teaching and prophecy. The emphasis is on God’s Word and the move of the Spirit.
+
+                    There will be 70 Sessions teaching God’s Word. The format will be teaching conferences in the morning where every aspect of the truth will be taught, followed by evening meetings where the ministration of the Word will be combined with the move of the Holy Spirit.
                     </p>
                 </div>
               </SwiperSlide>
@@ -126,13 +129,14 @@ export default class About extends Component {
                 <div className="artist__content">
                   <img
                       className="taltv__img"
-                      src={banner1}
+                      src={banner4}
                       alt="news"
                   />
-                  <h3>Frank Edward</h3>
-                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                     necessitatibusLorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  <h3>SOAR AWARDS 2022</h3>
+                  <p>
+                    The SOAR Awards, presented by SOAR Radio, is the first Gospel internet radio on the official Nielsen BDS National airplay panel. The forthcoming 2022 edition to be hosted by Todd Dulaney, will be honoring William Murphy, Dorinda Clark Cole, and John Hannah.
+
+                    Come March 7th, 2022, Sammie Okposo will be attending the SOAR awards at Rockford, Illinois, USA. Other international gospel artirtes billed for the award ceremony include Tasha Cobbs Leonard, Karen Clark Sheard, Kierra Sheard, Tim Bowman, Jonathan McReynolds, William Murphy, Da Truth, Anthony Brown, and more.
                     </p>
                 </div>
               </SwiperSlide>
--- src/components/Feautured Artist.jsx
+++ src/components/Feautured Artist.jsx
@@ -47,7 +47,13 @@ export default class FeauturedArtist extends Component {
                           />
                           <h3>Folabi-Nuel</h3>
                           <p>
-                              Folabi-Nuel is an award winning artist and a great gospel musician of our time.
+                              Folabi Nuel is a singer, songwriter, worship Leader, Recording artist and preacher with a mandate to bring people to a consciousness of who God His and how to be more like Him.. His debut single "God of Heaven" peaked at #1 on the Rhythm 93.7 Fm's "Holy Holla Top 5'' Gospel chart for several months.
+                              His debut album 'Good God'' which features some of Nigeria's finest musicians such as Florocka, Wole Oni also topped the charts with ''Good God feat. Florocka peaking at #1.
+                              He has since then featured on several world class events such as the yearly ''Make Music Lagos'' which features some of Nigeria's top acts.
+                              He has shared the stage with music greats such as Tim Godfrey & Travis Greene Tour.
+                              He was listed by Voyage ATL Magazine Atlanta as one of 10 inspiring creatives.
+                              He was also nominated for 2018 category at the just concluded Africa gospel and Media Awards UK.
+                              Folabi Nuel is an artist with a difference. combining jazz, rock and pop to create a unique sound that brings people to the consciousness of who God is.
                           </p>
                       </div>
                   </SwiperSlide>
@@ -59,9 +65,11 @@ export default class FeauturedArtist extends Component {
                               src={banner2}
                               alt="news"
                           />
-                          <h3>Guc</h3>
+                          <h3>Minister Guc</h3>
                           <p>
-                               Guc is an award winning artist and a great gospel musician of our time.
+                              GUC is a fast-rising gospel minister in songs and chants. He is the crooner of “All That Matters”  a song that is now a global anthem because of the solemn tune and the message behind it. The Eezee Tee Straboy has since then been making waves in the Gospel industry.
+
+                              A powerful worshipper whose songs have helped a lot of people connect and feel closer to God. His latest song Obinigwe is moving like wide fire across the Country and Africa
                           </p>
                       </div>
                   </SwiperSlide>
@@ -75,7 +83,8 @@ export default class FeauturedArtist extends Component {
                           />
                           <h3>Mercy Chinwo</h3>
                           <p>
-                              Mercy Chinwo is an award winning artist and a great gospel musician of our time.
+                              Mercy Chinwo is a Nigerian singer-songwriter, actress, and comedian. She started singing at age 8 in her church children’s choir. Eventually, in 2012, she came to the spotlight after winning in the popular talent show ‘Nigerian Idols’.
+                              Mercy is widely known for her hit single Excess Love (2018). The song earned her national and international recognition and sealed her status as one of the best vocalists in the Nigerian Gospel music ministry.
                           </p>
                       </div>
                   </SwiperSlide>
